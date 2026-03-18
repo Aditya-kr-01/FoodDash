9ab59ce6eb736f08a278299cbf8fe7d64c345450
Aditya-kr-01 <adk301104@gmail.com>
2026-03-18 23:10:28 +0530

    Refactor FoodController to follow REST standards and improve response handling

--- BACKEND/FoodApp/src/main/java/com/aditya/controller/FoodController.java
+++ BACKEND/FoodApp/src/main/java/com/aditya/controller/FoodController.java
@@ -3,18 +3,9 @@ package com.aditya.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.aditya.model.Food;
 import com.aditya.service.FoodService;
@@ -23,51 +14,49 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/food")
-@CrossOrigin(origins = "http://localhost:3000/")
-
+@CrossOrigin(origins = "http://localhost:3000")
 public class FoodController {
+
 	@Autowired
 	private FoodService fService;
-	@PostMapping("/add")
-	public ResponseEntity<String> addData(@Valid @RequestBody Food f)
-	{
-		String msg = "FOOD ADDED SUCCESSFULLY";
+
+	// CREATE
+	@PostMapping
+	public ResponseEntity<String> addData(@Valid @RequestBody Food f) {
 		fService.addData(f);
-		return new ResponseEntity<String>(msg, HttpStatus.CREATED);
-	}
-	@GetMapping("/fetch")
-	public ResponseEntity<List<Food>> getData()
-	{
-		List<Food> flist = fService.getData();
-		return new ResponseEntity<List<Food>>(flist, HttpStatus.OK);
-	}
-	@GetMapping("/fetch/{fid}")
-	public ResponseEntity<Food> getFidDetails(@PathVariable String fid)
-	{
-		Food f = fService.getFidDetails(fid);
-		return new ResponseEntity<Food>(f,HttpStatus.OK);
-	}
-	@DeleteMapping("/del/{fid}")
-	public ResponseEntity<String> deleteData(@PathVariable String fid)
-	{
-		String msg = "FOOD ITEM DELETED SUCCESSFULLY";
-		fService.deleteData(fid);
-		return new ResponseEntity<String>(msg, HttpStatus.OK);
-	}
-	@PutMapping("/upd/{fid}")
-	public ResponseEntity<String> updateData(@PathVariable String fid,@RequestBody Food f)
-	{
-		String msg=null;
-		Food fs=fService.updateData(fid, f);
-		if(fs!=null)
-		{
-			msg="FOOD ITEM UPDATED SUCCESSFULLY";
-		}
-		else
-		{
-			msg="UPDATE NOT DONE";
-		}
-		return new ResponseEntity<String>(msg,HttpStatus.OK);
+		return new ResponseEntity<>("Food added successfully", HttpStatus.CREATED);
 	}
 
+	// READ ALL
+	@GetMapping
+	public ResponseEntity<List<Food>> getData() {
+		return ResponseEntity.ok(fService.getData());
+	}
+
+	// READ BY ID
+	@GetMapping("/{fid}")
+	public ResponseEntity<Food> getFidDetails(@PathVariable String fid) {
+		Food f = fService.getFidDetails(fid);
+		if (f != null) {
+			return ResponseEntity.ok(f);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+
+	// DELETE
+	@DeleteMapping("/{fid}")
+	public ResponseEntity<String> deleteData(@PathVariable String fid) {
+		fService.deleteData(fid);
+		return ResponseEntity.ok("Food deleted successfully");
+	}
+
+	// UPDATE
+	@PutMapping("/{fid}")
+	public ResponseEntity<String> updateData(@PathVariable String fid, @Valid @RequestBody Food f) {
+		Food updated = fService.updateData(fid, f);
+		if (updated != null) {
+			return ResponseEntity.ok("Food updated successfully");
+		}
+		return new ResponseEntity<>("Food not found", HttpStatus.NOT_FOUND);
+	}
 }
