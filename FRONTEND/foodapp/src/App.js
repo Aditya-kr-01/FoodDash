c0a2b1147c256213cb0aa02e9187b6c3cd1e0450
Aditya-kr-01 <adk301104@gmail.com>
2026-03-19 22:41:01 +0530

    App.js

--- FRONTEND/foodapp/src/App.js
+++ FRONTEND/foodapp/src/App.js
@@ -1,49 +1,56 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import { Routes, Route } from 'react-router-dom';
+
 import AddFood from './component/AddFood';
 import FoodList from './component/FoodList';
 import DeleteFood from './component/DeleteFood';
 import UpdateFood from './component/UpdateFood';
 import SearchFood from './component/SearchFood';
-import Contact from './component/Contact';  
-import "bootstrap/dist/css/bootstrap.css";
-import { Routes,Route } from 'react-router-dom';
+import Contact from './component/Contact';
+
 import NavClient from './component/NavClient';
 import FoodListClient from './component/FoodListClient';
 import SearchFoodClient from './component/SearchFoodClient';
-import Billing from './component/Billing';  
+import Billing from './component/Billing';
+
 import Register from './component/Register';
-import Login  from './component/Login';
+import Login from './component/Login';
 import Home from './component/Home';
 import Nav from './component/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <h2>FOOD APP</h2>
-      <br />
+    <div>
+      <h1 style={{
+        textAlign: "center",
+        padding: "20px",
+        background: "#343a40",
+        color: "white",
+        margin: 0
+      }}>
+        🍔 FOOD DASHBOARD
+      </h1>
 
       <Routes>
-        {/* Home Route */}
-        <Route path='/' element={<Home/>}/>
-        <Route path='/nav' element={<Nav/>}/>
-        <Route path='/navclient' element={<NavClient/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/nav' element={<Nav />} />
+        <Route path='/navclient' element={<NavClient />} />
 
-        <Route path='/addfood' element={<AddFood/>}/>
-        <Route path='/foodlist' element={<FoodList/>}/>
-        <Route path='/delfood' element={<DeleteFood/>}/>
-        <Route path='/updfood' element={<UpdateFood/>}/>
-        <Route path='/sfood' element={<SearchFood/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/addfood' element={<AddFood />} />
+        <Route path='/foodlist' element={<FoodList />} />
+        <Route path='/delfood' element={<DeleteFood />} />
+        <Route path='/updfood' element={<UpdateFood />} />
+        <Route path='/sfood' element={<SearchFood />} />
+        <Route path='/contact' element={<Contact />} />
 
-        <Route path='/navclient' element={<NavClient/>}/>
-        <Route path='/foodlistclient' element={<FoodListClient/>}/>
-        <Route path='/sfoodclient' element={<SearchFoodClient/>}/>
-       
-        <Route path='/billing' element={<Billing/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/foodlistclient' element={<FoodListClient />} />
+        <Route path='/sfoodclient' element={<SearchFoodClient />} />
+        <Route path='/billing' element={<Billing />} />
+
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
-
     </div>
   );
 }
