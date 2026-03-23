ceb5b09d83bbbdd80d4fc159b717a449b157817f
Aditya-kr-01 <adk301104@gmail.com>
2026-03-23 20:19:12 +0530

    order main controller

--- BACKEND/FoodApp/src/main/java/com/aditya/controller/OrderMainController.java
+++ BACKEND/FoodApp/src/main/java/com/aditya/controller/OrderMainController.java
@@ -1,5 +1,20 @@
 package com.aditya.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.aditya.service.OrderMainService;
+
+@RestController
+@RequestMapping("/order")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class OrderMainController {
 
+    @Autowired
+    private OrderMainService oservice;
+
+    @PostMapping("/place/{uname}")
+    public String placeOrder(@PathVariable String uname) {
+        return oservice.placeOrder(uname);
+    }
 }
