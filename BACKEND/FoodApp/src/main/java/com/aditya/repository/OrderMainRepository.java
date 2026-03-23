11f5e731046dcaefc104d9b14824b875e1add017
Aditya-kr-01 <adk301104@gmail.com>
2026-03-23 20:15:17 +0530

    order main repository.java

--- BACKEND/FoodApp/src/main/java/com/aditya/repository/OrderMainRepository.java
+++ BACKEND/FoodApp/src/main/java/com/aditya/repository/OrderMainRepository.java
@@ -1,5 +1,10 @@
 package com.aditya.repository;
 
-public class OrderMainRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.aditya.model.OrderMain;
+
+@Repository
+public interface OrderMainRepository extends JpaRepository<OrderMain, Integer> {
 }
