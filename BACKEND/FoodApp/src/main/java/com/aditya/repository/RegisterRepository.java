9b4ca1f013f6a607e18904a239c32a69fbf068c3
Aditya-kr-01 <adk301104@gmail.com>
2026-03-26 21:38:14 +0530

    rrepo

--- BACKEND/FoodApp/src/main/java/com/aditya/repository/RegisterRepository.java
+++ BACKEND/FoodApp/src/main/java/com/aditya/repository/RegisterRepository.java
@@ -2,11 +2,15 @@ package com.aditya.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.aditya.model.Register;
+
 @Repository
 public interface RegisterRepository extends JpaRepository<Register, String>{
-	@Query("from Register where uname=:uname and pass=:pass")
-public Register checkLogin(String uname,String pass);
+
+    @Query("SELECT r FROM Register r WHERE r.uname = :uname AND r.pass = :pass")
+    public Register checkLogin(@Param("uname") String uname,
+                               @Param("pass") String pass);
 }
