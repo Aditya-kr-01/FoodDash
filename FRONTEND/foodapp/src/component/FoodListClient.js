0bae9fd98704511693475d0f6b74537a3d200f72
Aditya-kr-01 <adk301104@gmail.com>
2026-03-26 20:44:05 +0530

    foodlist client.js

--- FRONTEND/foodapp/src/component/FoodListClient.js
+++ FRONTEND/foodapp/src/component/FoodListClient.js
@@ -1,53 +1,38 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Food.css';
+
 function FoodListClient() {
-       let[food,setFood]=useState([]);
-    useEffect(()=>{ 
-           axios.get("http://localhost:1004/food/fetch")
-        .then((res)=>{ 
-            console.log(res.data);
-            setFood(res.data);
-           
-        })
-        .catch((error)=>{ 
-            console.log(error);
-        })
-    },[])
-  return (
-    <div>
-      { 
-        food.length>0 ? 
-        <div style={{width:"40%",margin:"50px auto" } }>
-         <table className='table table-hover'>
-           <thead className='table table-dark'>
-            <tr>
-                <th>FOOD ID</th>
-                <th>FOOD NAME</th>
-                <th>PRICE</th>
-                
-            </tr>
-           </thead>
-           <tbody>
-            {
-                food.map((element,index)=>{ 
-                    return(
-                        <tr>
-                            
-                             <td>{element.fid }</td>
-                            <td>{element.fname }</td>
-                              <td>{element.price }</td>
-                        </tr>
-                    )
-                })
-            }
-           </tbody>
-         </table>
+
+    const [food, setFood] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:1004/food/fetch")
+            .then((res) => {
+                setFood(res.data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    return (
+        <div className="food-container">
+            {food.map((item) => (
+                <div className="food-card" key={item.fid}>
+                    
+                    <img 
+                        src={item.imageUrl || "https://via.placeholder.com/150"} 
+                        alt="food"
+                    />
+
+                    <h3>{item.fname}</h3>
+                    <p className="category">{item.category}</p>
+                    <p className="price">₹{item.price}</p>
+
+                    <button>Add to Cart</button>
+                </div>
+            ))}
         </div>
-        :<h2 style={{color:"red" } }>NO FOOD INFORMATION</h2>
-      }
-    </div>
-  )
+    );
 }
 
-export default FoodListClient
+export default FoodListClient;
